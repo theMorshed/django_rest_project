00d11612aecb699670e077bfa90e9fe1b89d80b6
theMorshed <themorshedctg@gmail.com>
2023-08-16 12:23:09 +0600

    class based authentication added

--- django_api/views.py
+++ django_api/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from django_api.models import Product, ProductReview
 from django_api.serializers import ProductSerializer, ProductReviewSerializer
 
 # Create your views here.
 class ProductViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     
 class ProductReviewViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = ProductReview.objects.all()
     serializer_class = ProductReviewSerializer
