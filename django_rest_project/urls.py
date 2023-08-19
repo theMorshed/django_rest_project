18ad5147b943d13f518b27a62ccfb1ff0cd26ff6
theMorshed <themorshedctg@gmail.com>
2023-08-20 01:08:52 +0600

    create a new app called user_api for token authentication with login register and so on.

--- django_rest_project/urls.py
+++ django_rest_project/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('django_api.urls')),
+    path('account/', include('user_api.urls')),
 ]
