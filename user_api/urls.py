5912fe0687b66134aee21aeb6bd232d38b13f532
theMorshed <themorshedctg@gmail.com>
2023-08-22 13:03:45 +0600

    registration created with serializer

--- user_api/urls.py
+++ user_api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from user_api.views import RegistrationView
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
+    path('register/', RegistrationView.as_view(), name='register'),
 ]
--- user_api/views.py
+++ user_api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from user_api.serializers import RegistrationSerializer
+from rest_framework.authtoken.models import Token
 
 # Create your views here.
+class RegistrationView(APIView):
+    def post(self, request):
+        serializer = RegistrationSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status =  status.HTTP_201_CREATED)
+        return Response(serializer.errors, status =  status.HTTP_400_BAD_REQUEST)
