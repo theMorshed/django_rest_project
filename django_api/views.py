bd7647930cd0d7a601ddd6dd6ea7b5469e23da1c
theMorshed <themorshedctg@gmail.com>
2023-08-25 01:18:18 +0600

    install django filter and use it for filtering review

--- django_api/views.py
+++ django_api/views.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnl
 from django_api.models import Product, ProductReview
 from django_api.serializers import ProductSerializer, ProductReviewSerializer
 from django_api.permissions import AdminOrReadOnly, ReviewOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
 
 # Create your views here.
 class ProductViewSet(viewsets.ModelViewSet):
@@ -17,10 +18,13 @@ class ProductReviewViewSet(viewsets.ModelViewSet):
     # permission_classes = [IsAuthenticated]
     queryset = ProductReview.objects.all()
     serializer_class = ProductReviewSerializer
+    filter_backends = [DjangoFilterBackend]
+    # filterset_fields = ['user__username']
+    filterset_fields = ['rating', 'product']
     
-    def get_queryset(self):
-        queryset = ProductReview.objects.all()
-        username = self.request.query_params.get('username')
-        if username is not None:
-            queryset = queryset.filter(user__username=username)
-        return queryset
+    # def get_queryset(self):
+    #     queryset = ProductReview.objects.all()
+    #     username = self.request.query_params.get('username')
+    #     if username is not None:
+    #         queryset = queryset.filter(user__username__icontains=username)
+    #     return queryset
